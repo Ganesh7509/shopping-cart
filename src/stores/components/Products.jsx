29b2594b5f75af865dd59ef86da94f167e76009b
Ganesh7509 <kundeganesh52@gmail.com>
2024-12-29 01:37:15 +0530

    Initial commit

---
+++ src/stores/components/Products.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Mobiles from "./Mobiles";
+import Computers from "./Computers";
+import Watch from "./Watch";
+import Men from "./Men";
+import Furniture from "./Furniture";
+import AC from "./AC";
+import Kitchen from "./Kitchen";
+
+const Products = () => {
+  return (
+    <div>
+      <Mobiles />
+      <Computers />
+      <Watch />
+      <Men />
+      <Furniture />
+      <AC />
+      <Kitchen />
+    </div>
+  );
+};
+
+export default Products;
